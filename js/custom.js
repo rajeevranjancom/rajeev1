ea54ed7b69b88119edda690fafea0e3abc642575
Rajeev Ranjan <cs19rajeev@gmail.com>
2021-11-03 14:04:21 +0530

    files

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/*!
+
+ * Description: Personal Portfolio Template
+ * Author/Developer: Rajeev
+ * Author/Developer URL: https://themeforest.net/user/exill
+ * Version: v1.1.0
+ * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ */
+! function (n) {
+    "use strict";
+    n((function () {})), n(window).on("load", (function () {}))
+}(jQuery);
